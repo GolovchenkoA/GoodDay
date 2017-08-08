88561142811ba28cc008cbb377dcb9a7ab71b624
Golovchenko Artem <golovchenkoart@gmail.com>
2017-08-08 22:45:15 +0300

    VERSION 1.0.2 PRODUCTION

--- src/test/java/ua/golovchenko/artem/goodday/GreetingTestDataDrivenCSV.java
+++ src/test/java/ua/golovchenko/artem/goodday/GreetingTestDataDrivenCSV.java
@@ -1,6 +1,8 @@
 package ua.golovchenko.artem.goodday;
 
 import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import ua.golovchenko.artem.goodday.utils.CSVUtils;
 
 import java.time.LocalTime;
@@ -10,7 +12,13 @@ import static org.junit.Assert.*;
 
 public class GreetingTestDataDrivenCSV {
 
+    private static final Logger logger = LoggerFactory.getLogger(GreetingTestDataDrivenCSV.class);
+    private static String MORNING = "MORNING";
+    private static String DAY = "DAY";
+    private static String EVENING = "EVENING";
+    private static String NIGHT = "NIGHT";
     private static String TEST_DATA_FILE = "src/test/resources/DateTimeOfDay.csv";
+
     TimeOfDayIdentifier identifier = new TimeOfDayIdentifierImpl();
 
     @Test
@@ -26,24 +34,36 @@ public class GreetingTestDataDrivenCSV {
             String second = row[2];
             String timeOfDay = row[3];
 
-            if(timeOfDay.equals(TimeOfDayIdentifier.TimeOfDay.NIGHT)){
-                time = LocalTime.of(Integer.parseInt(hour),Integer.parseInt(minute), Integer.parseInt(second));
-                 assertEquals("NIGHT", identifier.isNight(time));
+            logger.debug("hour: {} minute: {} second: {} timeOfDay[from file]: {} ", hour, minute, second, timeOfDay);
+
+            time = LocalTime.of(Integer.parseInt(hour),Integer.parseInt(minute), Integer.parseInt(second));
+
+            if(timeOfDay.equals(NIGHT)){
+                 assertEquals(TimeOfDayIdentifier.TimeOfDay.NIGHT, identifier.getTimeOfDay(time));
+                 assertNotEquals(TimeOfDayIdentifier.TimeOfDay.MORINIG, identifier.getTimeOfDay(time));
+                 assertNotEquals(TimeOfDayIdentifier.TimeOfDay.DAY, identifier.getTimeOfDay(time));
+                 assertNotEquals(TimeOfDayIdentifier.TimeOfDay.EVENING, identifier.getTimeOfDay(time));
             }
 
-            if(timeOfDay.equals(TimeOfDayIdentifier.TimeOfDay.MORINIG)){
-                time = LocalTime.of(Integer.parseInt(hour),Integer.parseInt(minute), Integer.parseInt(second));
-                assertEquals("MORNING", identifier.isNight(time));
+            if(timeOfDay.equals(MORNING)){
+                assertNotEquals(TimeOfDayIdentifier.TimeOfDay.NIGHT, identifier.getTimeOfDay(time));
+                assertEquals(TimeOfDayIdentifier.TimeOfDay.MORINIG, identifier.getTimeOfDay(time));
+                assertNotEquals(TimeOfDayIdentifier.TimeOfDay.DAY, identifier.getTimeOfDay(time));
+                assertNotEquals(TimeOfDayIdentifier.TimeOfDay.EVENING, identifier.getTimeOfDay(time));
             }
 
-            if(timeOfDay.equals(TimeOfDayIdentifier.TimeOfDay.DAY)){
-                time = LocalTime.of(Integer.parseInt(hour),Integer.parseInt(minute), Integer.parseInt(second));
-                 assertEquals("DAY", identifier.isNight(time));
+            if(timeOfDay.equals(DAY)){
+                assertNotEquals(TimeOfDayIdentifier.TimeOfDay.NIGHT, identifier.getTimeOfDay(time));
+                assertNotEquals(TimeOfDayIdentifier.TimeOfDay.MORINIG, identifier.getTimeOfDay(time));
+                assertEquals(TimeOfDayIdentifier.TimeOfDay.DAY, identifier.getTimeOfDay(time));
+                assertNotEquals(TimeOfDayIdentifier.TimeOfDay.EVENING, identifier.getTimeOfDay(time));
             }
 
-             if(timeOfDay.equals(TimeOfDayIdentifier.TimeOfDay.EVENING)){
-                time = LocalTime.of(Integer.parseInt(hour),Integer.parseInt(minute), Integer.parseInt(second));
-                 assertEquals("EVENING", identifier.isNight(time));
+             if(timeOfDay.equals(EVENING)){
+                 assertNotEquals(TimeOfDayIdentifier.TimeOfDay.NIGHT, identifier.getTimeOfDay(time));
+                 assertNotEquals(TimeOfDayIdentifier.TimeOfDay.MORINIG, identifier.getTimeOfDay(time));
+                 assertNotEquals(TimeOfDayIdentifier.TimeOfDay.DAY, identifier.getTimeOfDay(time));
+                 assertEquals(TimeOfDayIdentifier.TimeOfDay.EVENING, identifier.getTimeOfDay(time));
             }
 
         }
